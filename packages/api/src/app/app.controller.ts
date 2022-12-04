220443d59a5070035af0b65435115bfc2f1261fc
Maulana Yusuf <ucupio@gmail.com>
2022-12-04 23:20:58 +0700

    setup nest api package

--- packages/api/src/app/app.controller.ts
+++ packages/api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,5 +9,21 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post()
+  create(
+    @Body()
+    { user_name, contact_phone }: { user_name: string; contact_phone: string }
+  ) {
+    return this.appService.add(user_name, contact_phone);
+  }
+
+  @Post()
+  setPassword(
+    @Body()
+    { id, password }: { id: string; password: string }
+  ) {
+    return this.appService.add(id, password);
   }
 }
--- packages/api/src/app/app.service.ts
+++ packages/api/src/app/app.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
+interface User {
+  user_id: string;
+  user_name: string;
+  city_id: number;
+  address: string;
+  contact_phone: string;
+  email: string;
+  confirmation_code: string;
+  password: string;
+  time_joined: string;
+}
+
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  private users: User[] = [];
+  getData(): User[] {
+    return this.users;
+  }
+
+  add(user_name: string, contact_phone: string): void {
+    this.users.push({
+      user_id: this.users.length.toString(),
+      user_name,
+      contact_phone,
+      address: '',
+      city_id: 0,
+      confirmation_code: '',
+      email: '',
+      password: '',
+      time_joined: '',
+    });
+  }
+
+  setPaassword(id: string, password: string): void {
+    this.users = this.users.map((user) => ({
+      ...user,
+      password: id === user.user_id ? password : user.password,
+    }));
   }
 }
