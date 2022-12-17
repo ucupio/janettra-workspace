2dfdcb1da90a9615c486cdb6e9c3fe0a6d8a9ff9
Maulana Yusuf. Azmi <maulana.azmi@primecash.co.id>
2022-12-17 20:32:16 +0700

    fix users and auth api

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/authentication')],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ucupio:tolokoy123@ucupio.lezbr.mongodb.net/?retryWrites=true&w=majority'
+    ),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- packages/api/src/auth/auth.controller.ts
+++ packages/api/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+}
